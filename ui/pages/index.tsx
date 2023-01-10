327935ca5396603eea4477e9b0bcca5705a68b10
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-01-10 16:47:20 +0100

    clean nextjs react project

---
+++ ui/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Inter } from "@next/font/google";
+import { Fragment } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <Fragment>
+      <h1>Init</h1>
+    </Fragment>
+  );
+}
