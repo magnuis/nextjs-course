e68cabfa69f4c3027cf7f85110b4dc5d8a4d953d
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-01-17 12:54:53 +0100

    n

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -12,10 +12,7 @@ function BlogList({ posts }: Props) {
   return (
     <div>
       <hr className="border-[#F7AB0A] mb-10" />
-      <div
-        className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16"
-        pb-24
-      >
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {" "}
         {posts.map((post) => (
           <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
